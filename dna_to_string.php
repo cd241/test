9ff20faf0110934e3ee446f2959ff05918918f64
Chiragh Dewan <chiraghdewan@gmail.com>
2018-12-09 12:09:58 -0800

    changes

--- dna_to_string.php
+++ dna_to_string.php
@@ -33,8 +33,8 @@
     } else {
         if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
             $convert = exec('python /usr/local/lib/python2.7/site-packages/dna/dna.py -d /var/www/html/'.$target_file);
-            $rename = rename("/var/www/html/".$target_file, "/var/www/html/".$target_file.".decoded");
-            $get_file_contents = file_get_contents("/var/www/html/".$target_file.".decoded");
+            $rename = rename("/var/www/html/".$target_file, "/var/www/html/test.decoded");
+            $get_file_contents = file_get_contents("/var/www/html/test.decoded");
             echo "Converted String: ".$get_file_contents;
             // echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
         } else {
