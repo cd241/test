b1c92ccc929ddf762be2e7d19c536e9aaa721cdb
Chiragh Dewan <chiraghdewan@gmail.com>
2018-12-09 12:06:24 -0800

    changes

--- dna_to_string.php
+++ dna_to_string.php
@@ -34,7 +34,7 @@
         if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
             $convert = exec('python /usr/local/lib/python2.7/site-packages/dna/dna.py -d /var/www/html/'.$target_file);
             $rename = rename("/var/www/html/".$target_file, "/var/www/html/".$target_file.".decoded");
-            $get_file_contents = file_get_contents("/var/www/html/".$rename);
+            $get_file_contents = file_get_contents("/var/www/html/".$target_file.".decoded");
             echo "Converted String: ".$get_file_contents;
             // echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
         } else {
