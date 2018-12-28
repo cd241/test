ba1aa510388bedae9606c6eecccd076f628213c4
Chiragh Dewan <chiraghdewan@gmail.com>
2018-12-28 09:25:58 -0600

    changes

--- order_tag.php
+++ order_tag.php
@@ -1,99 +1,10 @@
-<?php
-	
-	error_reporting(E_ALL);
-    ini_set('display_errors', 1);
-
-    $servername = "localhost";
-	$username = "root";
-	$password = "Duelmasters241!";
-
-	try {
-	    $conn = new PDO("mysql:host=$servername;dbname=order_tag", $username, $password);
-	    // set the PDO error mode to exception
-	    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	    //echo "Connected successfully"; 
-	}
-	
-	catch(PDOException $e) {
-		echo "Connection failed: " . $e->getMessage();
-	}
-
-	// Generates random 6 character long alphanumeric value
-    function generateRandomString($length = 6) {
-        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $charactersLength = strlen($characters);
-        $randomString = '';
-        for ($i = 0; $i < $length; $i++) {
-            $randomString .= $characters[rand(0, $charactersLength - 1)];
-        }
-        return $randomString;
-    }
-
-    if(!empty($cus_name = $_GET['cus_name'];) && !empty($cus_number = $_GET['cus_number'];) && !empty($chasis = $_GET['chasis'];) && !empty($host = $_GET['host'];) && !empty($ppg = $_GET['ppg'];) && !empty($cus_location = $_GET['cus_location'];) && !empty($geotag = $_GET['geotag'];) && !empty($tag = $_GET['tag'];)) {
-
-    	$randomString = generateRandomString();
-
-	    $updatedString = "CRV".$randomString."CRV";
-
-	    $myfile = fopen("uploads/".$randomString.".txt", "w") or die("Unable to open file!");
-
-	   	fwrite($myfile, $updatedString);
-
-	   	fclose($myfile);
-
-	   	$convert = exec('python /usr/local/lib/python2.7/site-packages/dna/dna.py -e /var/www/html/uploads/'.$randomString.'.txt');
-	    $get_file_contents = file_get_contents("/var/www/html/uploads/".$randomString.".txt.dna");
-
-	    $dna_string = $get_file_contents;
-
-	    $length = strlen($get_file_contents);
-
-	    if($length > 50 && $length < 120) {
-	        echo "Good String length";
-	    }
-
-	    if($length > 120) {
-	        echo "String length over 120 characters";
-	    }
-
-	    if($length < 50) {
-	        echo "String length less than 50 characters";
-	    }
-
-	    $a = "AAA";
-	    $c = "CCC";
-	    $g = "GGG";
-	    $t = "TTT";
-
-	    if(substr_count($get_file_contents, $a) > 0) {
-	        echo "AAA is present";
-	    }
-
-	    if(substr_count($get_file_contents, $c) > 0) {
-	        echo "CCC is present";
-	    }
-
-	    if(substr_count($get_file_contents, $g) > 0) {
-	        echo "GGG is present";
-	    }
-
-	    if(substr_count($get_file_contents, $t) > 0) {
-	        echo "TTT is present";
-	    }
-    }
-
-    else {
-    	echo "Enter all details";
-    }
-?>
-
 <!DOCTYPE html>
 <html>
 <body>
 
     <br>
 
-    <form action="order_tag.php" method="GET">
+    <form action="order_tag_backend.php" method="POST">
     	<label>Customer Name</label>
 	    <input type="text" name="cus_name">
 
